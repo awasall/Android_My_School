1538fe61ba3839a143f1b4f81df04febdd941f4c
AWA SALL [SNT DSI/DAC/DIF/RAD] <TMP_SALL50857@orange-sonatel.com>
2021-05-03 19:14:46 +0000

    test with emulator

--- app/src/main/java/com/example/myschool/MainActivity.java
+++ app/src/main/java/com/example/myschool/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.myschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtLogin, txtPassword;
+    private Button btnConnect, btnSignUp;
+    private String login, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLogin= findViewById(R.id.txtLogin);
+        txtPassword= findViewById(R.id.txtPassword);
+        btnConnect= findViewById(R.id.btnConnect);
+        btnSignUp= findViewById(R.id.btnSign_up);
+        //Gerer le clique sur le button
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login =txtLogin.getText().toString().trim();
+                password =txtPassword.getText().toString().trim();
+
+                if(login.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, getString(R.string.error_fields), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent intent=new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
 }
