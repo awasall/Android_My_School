398ba3f59c845cba580768484ea76b09e6304f53
AWA SALL [SNT DSI/DAC/DIF/RAD] <TMP_SALL50857@orange-sonatel.com>
2021-06-21 18:44:55 +0000

    àjour

---
+++ app/src/main/java/com/example/myschool/InscriptionFragment.java
@@ -0,0 +1,23 @@
+package com.example.myschool;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InscriptionFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_inscription, container, false);
+    return view;
+    }
+
+}
